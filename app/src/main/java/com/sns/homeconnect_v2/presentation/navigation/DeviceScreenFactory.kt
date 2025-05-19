f5885a79a3fd972d8d6e38bf178f3dd4e3a1357b
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-05-19 14:18:38 +0700

    Refactor: Temporarily remove navigation for device types 2 and 3
    This commit comments out the navigation entries for device types 2 and 3 in the `DeviceScreenFactory`. This temporarily removes the ability to navigate to the detail screens for these device types.
    
    #21

--- app/src/main/java/com/sns/homeconnect_v2/presentation/navigation/DeviceScreenFactory.kt
+++ app/src/main/java/com/sns/homeconnect_v2/presentation/navigation/DeviceScreenFactory.kt
@@ -11,8 +11,8 @@ import com.sns.homeconnect_v2.presentation.screen.iot_device.FireAlarmDetailScre
 object DeviceScreenFactory {
     private val screenMap: Map<Int, @Composable (NavHostController, Int?) -> Unit> = mapOf(
         1 to { navController, id -> FireAlarmDetailScreen(navController, id) },
-        2 to { navController, id -> DeviceDetailScreen(navController,  id) },
-        3 to { navController, id -> DeviceDetailScreen(navController,  id) }
+//        2 to { navController, id -> DeviceDetailScreen(navController,  id) },
+//        3 to { navController, id -> DeviceDetailScreen(navController,  id) }
     )
 
     fun getScreen(typeID: Int): @Composable (NavHostController, Int?) -> Unit {

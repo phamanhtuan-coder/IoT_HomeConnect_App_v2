12c1d830bd15e6991caf755eead7ca87991db147
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-05-19 15:41:52 +0700

    feat: Create InfoRow Composable
    This commit introduces a new composable function `InfoRow`.
    
    The `InfoRow` composable is designed to display a row of information typically consisting of a label, a value, a unit, and a state indicator. The state indicator is represented by a colored circle with text inside.
    
    Key characteristics of `InfoRow`:
    - Takes `label`, `value`, `unit`, `stateColor`, and `stateText` as parameters.
    - Uses `Row` layout to arrange elements.
    - The `label` and the `value`/`unit` pair are weighted for responsive layout.
    - The `value` is displayed with a bold font.
    - The state is represented by a circular `Box` with a background color (`stateColor`) and text (`stateText`).
    - The composable is wrapped in `IoTHomeConnectAppTheme` to utilize the application's theme.
    
    #22

---
+++ app/src/main/java/com/sns/homeconnect_v2/presentation/component/InfoRow.kt
@@ -0,0 +1,80 @@
+package com.sns.homeconnect_v2.presentation.component
+
+import IoTHomeConnectAppTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+// Hàm hiển thị từng hàng thông tin
+fun InfoRow(label: String, value: String, unit: String, stateColor: Color, stateText: String)
+{
+    IoTHomeConnectAppTheme {
+        val colorScheme = MaterialTheme.colorScheme
+        Row(
+            modifier = Modifier
+                .wrapContentWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = label,
+                fontSize = 16.sp,
+                modifier = Modifier.weight(1f),
+                color = colorScheme.onBackground,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Row(
+                modifier = Modifier.weight(2f),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = value,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = colorScheme.onBackground,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = unit,
+                    fontSize = 12.sp,
+                    modifier = Modifier.offset(y = 3.dp),
+                    color = colorScheme.onBackground,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .size(32.dp)
+                    .clip(CircleShape)
+                    .background(stateColor),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stateText,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
+}

0e8ea2e83ac726e8eb26abf829a29f69e29705d3
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-05-25 20:40:21 +0700

    feat(model): Create DeviceStatusCardData model
    This commit introduces a new data class `DeviceStatusCardData` to represent the information displayed on a device status card.
    
    The class includes the following properties:
    - `title`: The name or title of the device.
    - `type`: The type of the device (e.g., "Air Conditioner", "Light").
    - `status`: A boolean indicating the current status of the device (e.g., on/off).
    - `power`: A string representing the power consumption or setting.
    - `temp`: A string representing the temperature reading or setting.
    
    #33

---
+++ app/src/main/java/com/sns/homeconnect_v2/presentation/model/DeviceStatusCardData.kt
@@ -0,0 +1,10 @@
+package com.sns.homeconnect_v2.presentation.model
+
+data class DeviceStatusCardData(
+    val title: String,
+    val type: String,
+    val status: Boolean,
+    val power: String,
+    val temp: String
+)
+

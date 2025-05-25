a2484e9dd8ce479e944dd9167b3e39933b714aec
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-05-25 20:41:37 +0700

    fix(widget): Remove unused import in CustomTabRow
    This commit removes the unused import `androidx.compose.material3.TabRow` from the `CustomTabRow.kt` file. The component uses `ScrollableTabRow` instead.
    
    #33

--- app/src/main/java/com/sns/homeconnect_v2/presentation/component/widget/CustomTabRow.kt
+++ app/src/main/java/com/sns/homeconnect_v2/presentation/component/widget/CustomTabRow.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text

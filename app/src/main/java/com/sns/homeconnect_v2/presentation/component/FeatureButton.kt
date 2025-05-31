4c5e16f247a17abd5c01b07f30968ae0d8e28146
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-05-31 08:45:53 +0700

    feat(component): Create reusable FeatureButton composable
    This commit introduces a new composable function `FeatureButton`.
    
    Key changes:
    - Created `FeatureButton.kt` which defines a composable function for displaying a feature button.
    - The `FeatureButton` takes a `FeatureButtonItem` (containing an icon, label, and onClick action), modifier, icon color, and background color as parameters.
    - It displays an icon within a rounded square background and a label below it.
    - Added a `@Preview` function `FeatureButtonPreview` to showcase the button with sample data.
    
    #67

---
+++ app/src/main/java/com/sns/homeconnect_v2/presentation/component/FeatureButton.kt
@@ -0,0 +1,77 @@
+package com.sns.homeconnect_v2.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material3.Icon
+import androidx.wear.compose.material3.Text
+import com.sns.homeconnect_v2.presentation.model.FeatureButtonItem
+
+@Composable
+fun FeatureButton(
+    item: FeatureButtonItem,
+    modifier: Modifier = Modifier,
+    iconColor: Color = MaterialTheme.colorScheme.primary,
+    backgroundColor: Color = Color(0xFFD8E4E8),
+) {
+    Column(
+        modifier = Modifier
+            .width(86.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box (
+            modifier = modifier
+                .size(86.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .background(backgroundColor)
+                .clickable { item.onClick() },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = item.icon,
+                contentDescription = null,
+                tint = iconColor,
+                modifier = Modifier.size(51.dp)
+            )
+        }
+        Spacer(Modifier.height(8.dp))
+        Text(
+            text = item.label,
+            color = Color(0xFF444444),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            maxLines = 2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun FeatureButtonPreview() {
+    val item = FeatureButtonItem(
+        icon = Icons.Default.Add,
+        label = "Thêm thiết bị",
+        onClick = {})
+
+    FeatureButton(item = item)
+}

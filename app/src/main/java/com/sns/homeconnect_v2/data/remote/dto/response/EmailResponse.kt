abf84d7f0d72540e1db8122bf9812fefab02ed48
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-04 16:41:44 +0700

    Refactor(reponse): Update EmailResponse data class
    This commit updates the `EmailResponse` data class to better reflect the server's response structure.
    
    Key changes:
    - Replaced `success: Boolean` and `exists: Boolean?` fields with `status: String?` and `code: String?`.
    - The `message: String` field remains unchanged.
    
    The new fields `status` and `code` are expected to provide more detailed information about the email operation, potentially aligning with HTTP status codes or custom server codes.
    
    #76

--- app/src/main/java/com/sns/homeconnect_v2/data/remote/dto/response/EmailResponse.kt
+++ app/src/main/java/com/sns/homeconnect_v2/data/remote/dto/response/EmailResponse.kt
@@ -1,7 +1,7 @@
 package com.sns.homeconnect_v2.data.remote.dto.response
 
 data class EmailResponse(
-    val success: Boolean,
-    val exists: Boolean?,
+    val status: String?,
+    val code: String?,
     val message: String
 )

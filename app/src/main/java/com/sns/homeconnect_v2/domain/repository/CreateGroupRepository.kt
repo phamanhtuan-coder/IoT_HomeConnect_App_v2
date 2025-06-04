f1bed423cfd7b51e91f3442a241f4d60050cfdab
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-04 17:22:52 +0700

    feat(repository): Add `getMyGroups` method to `GroupRepository`
    This commit introduces a new method `getMyGroups` to the `GroupRepository` interface.
    
    The `getMyGroups` method is a suspend function that is expected to return a `Result` containing a list of `GroupResponse` objects, representing the groups associated with the current user.
    
    #81

--- app/src/main/java/com/sns/homeconnect_v2/domain/repository/CreateGroupRepository.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/CreateGroupRepository.kt
@@ -2,7 +2,9 @@ package com.sns.homeconnect_v2.domain.repository
 
 import com.sns.homeconnect_v2.data.remote.dto.base.CreateGroupResponse
 import com.sns.homeconnect_v2.data.remote.dto.request.CreateGroupRequest
+import com.sns.homeconnect_v2.data.remote.dto.response.GroupResponse
 
 interface GroupRepository {
     suspend fun createGroup(request: CreateGroupRequest): Result<CreateGroupResponse>
+    suspend fun getMyGroups(): Result<List<GroupResponse>>
 }

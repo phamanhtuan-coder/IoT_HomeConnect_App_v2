d8a8b75761131e1065e33963e8b5bb06ad49d64e
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-04 17:25:12 +0700

    feat(group): Implement GetMyGroupsUseCase
    This commit introduces the `GetMyGroupsUseCase` which is responsible for fetching the groups associated with the current user.
    
    Key changes:
    - Created `GetMyGroupsUseCase.kt`.
    - The use case takes `GroupRepository` as a dependency.
    - The `invoke` operator function calls the `getMyGroups` method on the repository to retrieve a list of `GroupResponse` objects.
    
    #81

---
+++ app/src/main/java/com/sns/homeconnect_v2/domain/usecase/group/GetMyGroupsUseCase.kt
@@ -0,0 +1,14 @@
+package com.sns.homeconnect_v2.domain.usecase.group
+
+import com.sns.homeconnect_v2.data.remote.dto.response.GroupResponse
+import com.sns.homeconnect_v2.domain.repository.GroupRepository
+import jakarta.inject.Inject
+
+class GetMyGroupsUseCase @Inject constructor(
+    private val repository: GroupRepository
+) {
+    suspend operator fun invoke(): Result<List<GroupResponse>> {
+        return repository.getMyGroups()
+    }
+}
+

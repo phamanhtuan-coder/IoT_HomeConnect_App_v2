89504cf3f15e4189cfc57ac4d5787b559b6c8b41
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-05 17:53:26 +0700

    Refactor(repository): Rename CreateGroupRepository to GroupRepository and Add Update Functionality
    This commit renames the `CreateGroupRepository` interface to `GroupRepository` to better reflect its expanded responsibilities.
    
    Additionally, a new `updateGroup` method has been added to the `GroupRepository` interface. This method allows for updating group information by providing a `groupId` and an `UpdateGroupRequest`, and it returns an `UpdateGroupResponse`.
    
    #85

--- app/src/main/java/com/sns/homeconnect_v2/domain/repository/CreateGroupRepository.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/CreateGroupRepository.kt
@@ -2,7 +2,10 @@ package com.sns.homeconnect_v2.domain.repository
 
 import com.sns.homeconnect_v2.data.remote.dto.base.CreateGroupResponse
 import com.sns.homeconnect_v2.data.remote.dto.request.CreateGroupRequest
+import com.sns.homeconnect_v2.data.remote.dto.request.UpdateGroupRequest
+import com.sns.homeconnect_v2.data.remote.dto.response.UpdateGroupResponse
 
 interface GroupRepository {
     suspend fun createGroup(request: CreateGroupRequest): Result<CreateGroupResponse>
+    suspend fun updateGroup(groupId: Int, request: UpdateGroupRequest): UpdateGroupResponse
 }

5c680772098536ffc4f963c32b12f1b5f8c04b6e
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-06 13:10:00 +0700

    refactor(group): Update GroupIcon sealed class
    This commit refactors the `GroupIcon` sealed class.
    
    - The constructor for `GroupIcon` now accepts a `name` parameter in addition to `label`.
    - The `Vector` and `Image` subclasses have been updated to accept `iconName` and `iconLabel` parameters, passing them to the `GroupIcon` constructor.
    - `Vector` and `Image` are now `data class`es.
    
    #87

--- app/src/main/java/com/sns/homeconnect_v2/data/remote/dto/base/GroupIcon.kt
+++ app/src/main/java/com/sns/homeconnect_v2/data/remote/dto/base/GroupIcon.kt
@@ -3,8 +3,12 @@ package com.sns.homeconnect_v2.data.remote.dto.base
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class GroupIcon(val label: String) {
-    class Vector(val icon: ImageVector, label: String) : GroupIcon(label)
-    class Image(val painter: Painter, label: String) : GroupIcon(label)
+sealed class GroupIcon(val label: String, val name: String) {
+    data class Vector(val icon: ImageVector, val iconName: String, val iconLabel: String)
+        : GroupIcon(iconLabel, iconName)
+
+    data class Image(val painter: Painter, val iconName: String, val iconLabel: String)
+        : GroupIcon(iconLabel, iconName)
 }
+
 

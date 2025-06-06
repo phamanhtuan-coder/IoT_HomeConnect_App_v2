88672bdb905721d5595dd00b836e3678aa1eed9d
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-06 17:00:49 +0700

    feat(repository): Add getHousesByGroupId to HouseRepository
    This commit introduces a new method `getHousesByGroupId` to the `HouseRepository` interface.
    
    This method will allow fetching a list of `HouseWithSpacesResponse` objects based on a provided `groupId`.
    
    #87

--- app/src/main/java/com/sns/homeconnect_v2/domain/repository/HouseRepository.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/HouseRepository.kt
@@ -1,5 +1,6 @@
 package com.sns.homeconnect_v2.domain.repository
 
+import com.sns.homeconnect_v2.data.remote.dto.response.HouseWithSpacesResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.house.CreateHouseRequest
 import com.sns.homeconnect_v2.data.remote.dto.response.house.CreateHouseResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.house.HouseResponse
@@ -9,9 +10,9 @@ import com.sns.homeconnect_v2.data.remote.dto.response.house.UpdateHouseResponse
 
 
 interface HouseRepository {
-
     suspend fun getListHome(): List<HouseResponse>
     suspend fun getHouses(): List<HousesListResponse>
     suspend fun updateHouse(houseID: Int, request: UpdateHouseRequest): UpdateHouseResponse
     suspend fun createHouse(request: CreateHouseRequest): CreateHouseResponse
+    suspend fun getHousesByGroupId(groupId: Int): List<HouseWithSpacesResponse>
 }

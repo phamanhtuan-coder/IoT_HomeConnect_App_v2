342ea66344e522d689978f74cdb7f9ab728bf008
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-06 16:27:40 +0700

    refactor(repository): Rename CreateGroupRepository to GroupRepository and add functionality
    This commit renames `CreateGroupRepository` to `GroupRepository` to better reflect its broader scope.
    
    Additionally, the `GroupRepository` interface has been updated to include a new function:
    - `addGroupMember(request: AddGroupMemberRequest): Result<UserGroupResponse>`: This function allows for adding a member to a group.
    
    #87

--- app/src/main/java/com/sns/homeconnect_v2/domain/repository/GroupRepository.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/GroupRepository.kt
@@ -6,10 +6,13 @@ import com.sns.homeconnect_v2.data.remote.dto.response.GroupResponse
 import com.sns.homeconnect_v2.data.remote.dto.request.UpdateGroupRequest
 import com.sns.homeconnect_v2.data.remote.dto.response.MemberResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.UpdateGroupResponse
+import com.sns.homeconnect_v2.data.remote.dto.request.AddGroupMemberRequest
+import com.sns.homeconnect_v2.data.remote.dto.response.UserGroupResponse
 
 interface GroupRepository {
     suspend fun createGroup(request: CreateGroupRequest): Result<CreateGroupResponse>
     suspend fun getMyGroups(): Result<List<GroupResponse>>
     suspend fun updateGroup(groupId: Int, request: UpdateGroupRequest): UpdateGroupResponse
     suspend fun getGroupMembers(groupId: Int): Result<List<MemberResponse>>
+    suspend fun addGroupMember(request: AddGroupMemberRequest): Result<UserGroupResponse>
 }

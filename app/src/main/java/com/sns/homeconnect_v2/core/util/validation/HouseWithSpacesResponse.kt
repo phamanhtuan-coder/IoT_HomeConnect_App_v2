c2a4b7862e9d1fcdac447b015ace61e112f5b329
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-09 11:49:04 +0700

    feat(validation): Add user role to HouseUi model
    This commit updates the `HouseUi` data model and its mapping function `toHouseUi` to include the user's role within a house.
    
    Key changes:
    - Added a `role` property of type `String` to the `HouseUi` data class.
    - Modified the `toHouseUi` extension function for `HouseWithSpacesResponse` to accept a `role` parameter and assign it to the corresponding property in the `HouseUi` object.
    
    #90

--- app/src/main/java/com/sns/homeconnect_v2/core/util/validation/HouseWithSpacesResponse.kt
+++ app/src/main/java/com/sns/homeconnect_v2/core/util/validation/HouseWithSpacesResponse.kt
@@ -3,11 +3,12 @@ package com.sns.homeconnect_v2.core.util.validation
 import com.sns.homeconnect_v2.data.remote.dto.response.HouseWithSpacesResponse
 import com.sns.homeconnect_v2.presentation.model.HouseUi
 
-fun HouseWithSpacesResponse.toHouseUi(): HouseUi {
+fun HouseWithSpacesResponse.toHouseUi(role: String): HouseUi {
     return HouseUi(
         id         = house_id,
         name       = house_name,
         spaces     = spaces.size,
+        role       = role,
         isRevealed = false,
         icon       = getIconByName(icon_name),
         iconColor  = parseColorOrDefault(icon_color)

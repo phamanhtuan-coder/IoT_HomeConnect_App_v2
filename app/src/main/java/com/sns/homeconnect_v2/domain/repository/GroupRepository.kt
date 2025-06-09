447bbffb1ceaeae42d2185ba829139199a459ccb
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-09 16:15:25 +0700

    feat(repository): Add `updateGroupMemberRole` to GroupRepository
    This commit introduces the `updateGroupMemberRole` function to the `GroupRepository` interface. This function allows updating the role of a member within a group.
    
    Key changes:
    - Added the `updateGroupMemberRole` function to `GroupRepository.kt`.
    - This function takes `groupId`, `accountId`, and `role` as parameters and returns a `Result<UpdateGroupMemberRoleResponse>`.
    
    #92

--- app/src/main/java/com/sns/homeconnect_v2/domain/repository/GroupRepository.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/GroupRepository.kt
@@ -1,12 +1,13 @@
 package com.sns.homeconnect_v2.domain.repository
 
 import com.sns.homeconnect_v2.data.remote.dto.base.CreateGroupResponse
-import com.sns.homeconnect_v2.data.remote.dto.request.CreateGroupRequest
-import com.sns.homeconnect_v2.data.remote.dto.response.GroupResponse
-import com.sns.homeconnect_v2.data.remote.dto.request.UpdateGroupRequest
-import com.sns.homeconnect_v2.data.remote.dto.response.MemberResponse
-import com.sns.homeconnect_v2.data.remote.dto.response.UpdateGroupResponse
 import com.sns.homeconnect_v2.data.remote.dto.request.AddGroupMemberRequest
+import com.sns.homeconnect_v2.data.remote.dto.request.CreateGroupRequest
+import com.sns.homeconnect_v2.data.remote.dto.request.UpdateGroupRequest
+import com.sns.homeconnect_v2.data.remote.dto.response.GroupResponse
+import com.sns.homeconnect_v2.data.remote.dto.response.MemberResponse
+import com.sns.homeconnect_v2.data.remote.dto.response.UpdateGroupMemberRoleResponse
+import com.sns.homeconnect_v2.data.remote.dto.response.UpdateGroupResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.UserGroupResponse
 
 interface GroupRepository {
@@ -15,4 +16,9 @@ interface GroupRepository {
     suspend fun updateGroup(groupId: Int, request: UpdateGroupRequest): UpdateGroupResponse
     suspend fun getGroupMembers(groupId: Int): Result<List<MemberResponse>>
     suspend fun addGroupMember(request: AddGroupMemberRequest): Result<UserGroupResponse>
+    suspend fun updateGroupMemberRole(
+        groupId: Int,
+        accountId: String,
+        role: String
+    ): Result<UpdateGroupMemberRoleResponse>
 }

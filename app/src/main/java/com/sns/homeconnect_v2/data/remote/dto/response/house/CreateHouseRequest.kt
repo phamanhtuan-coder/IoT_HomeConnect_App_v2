e8e1548c00f5431849adca9e5cc238050df86572
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-09 11:43:19 +0700

    fix(house): Remove CreateHouseRequest data class
    The `CreateHouseRequest` data class has been removed from the `com.sns.homeconnect_v2.data.remote.dto.response.house` package. This class was previously used to represent the request body for creating a new house.
    
    #90

--- app/src/main/java/com/sns/homeconnect_v2/data/remote/dto/response/house/CreateHouseRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.sns.homeconnect_v2.data.remote.dto.response.house
-
-data class CreateHouseRequest(
-    val name: String,
-    val address: String,
-    val iconName: String,
-    val iconColor: String
-)

19ea1b959679693392b5db3daf42d53508893b41
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-10 16:57:59 +0700

    refactor(auth): Remove NewPasswordUseCase
    The `NewPasswordUseCase` has been commented out, effectively removing it from the project. This use case was responsible for handling the new password functionality through the `AuthRepository`.
    
    #94

--- app/src/main/java/com/sns/homeconnect_v2/domain/usecase/auth/NewPasswordUseCase.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/usecase/auth/NewPasswordUseCase.kt
@@ -4,15 +4,15 @@ import com.sns.homeconnect_v2.data.remote.dto.response.NewPasswordResponse
 import com.sns.homeconnect_v2.domain.repository.AuthRepository
 import javax.inject.Inject
 
-class NewPasswordUseCase @Inject constructor(
-    private val authRepository: AuthRepository,
-) {
-    suspend operator fun invoke(email: String, password: String): Result<NewPasswordResponse> {
-        return try {
-            val response = authRepository.newPassword(email, password)
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-}
+//class NewPasswordUseCase @Inject constructor(
+//    private val authRepository: AuthRepository,
+//) {
+//    suspend operator fun invoke(email: String, password: String): Result<NewPasswordResponse> {
+//        return try {
+//            val response = authRepository.newPassword(email, password)
+//            Result.success(response)
+//        } catch (e: Exception) {
+//            Result.failure(e)
+//        }
+//    }
+//}

e729f414c312ea0cc9459aaf3a5d5c15f0836f1c
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-11 11:08:21 +0700

    Refactor(response): Rename RecoveryPasswordResponse to ForgotPasswordResponse
    This commit renames the data class `RecoveryPasswordResponse` to `ForgotPasswordResponse` to align with the AWS Cognito API naming for password recovery functionality.
    
    #98

--- app/src/main/java/com/sns/homeconnect_v2/data/remote/dto/response/RecoveryPasswordResponse.kt
+++ app/src/main/java/com/sns/homeconnect_v2/data/remote/dto/response/RecoveryPasswordResponse.kt
@@ -1,6 +1,6 @@
 package com.sns.homeconnect_v2.data.remote.dto.response
 
-data class RecoveryPasswordResponse(
+data class ForgotPasswordResponse(
     val success: Boolean,
     val message: String
 )

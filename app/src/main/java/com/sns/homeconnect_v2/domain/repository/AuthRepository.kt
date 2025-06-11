7a6c0820ed976956a1b5760f55dbe06091cee7e3
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-11 11:12:28 +0700

    Refactor(repository): Rename `recoveryPassword` to `forgotPassword`
    This commit renames the `recoveryPassword` method to `forgotPassword` in `AuthRepositoryImpl` and `AuthRepository` for clarity.
    The corresponding response type `RecoveryPasswordResponse` is also renamed to `ForgotPasswordResponse`.
    
    Key changes:
    - Renamed method `recoveryPassword` to `forgotPassword` in `AuthRepositoryImpl.kt`.
    - Updated the return type of the renamed method to `ForgotPasswordResponse`.
    
    #98

--- app/src/main/java/com/sns/homeconnect_v2/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/AuthRepository.kt
@@ -2,9 +2,9 @@ package com.sns.homeconnect_v2.domain.repository
 
 import com.sns.homeconnect_v2.data.remote.dto.request.RegisterRequest
 import com.sns.homeconnect_v2.data.remote.dto.response.CheckEmailResponse
+import com.sns.homeconnect_v2.data.remote.dto.response.ForgotPasswordResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.LoginResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.NewPasswordResponse
-import com.sns.homeconnect_v2.data.remote.dto.response.RecoveryPasswordResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.RegisterResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.User
 
@@ -15,5 +15,5 @@ interface AuthRepository {
     suspend fun getInfoProfile(): User
     suspend fun newPassword(email: String, password: String): NewPasswordResponse
     suspend fun checkEmail(email: String): CheckEmailResponse
-    suspend fun recoveryPassword(email: String, newPassword: String): RecoveryPasswordResponse
+    suspend fun forgotPassword(email: String, newPassword: String): ForgotPasswordResponse
 }

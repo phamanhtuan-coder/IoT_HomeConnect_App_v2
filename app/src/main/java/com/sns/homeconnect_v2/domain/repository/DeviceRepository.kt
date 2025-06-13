557883161b6d3c666270dc691ea73e8e6d065e03
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-13 14:48:05 +0700

    feat(repository): Add endpoint to retrieve user-owned devices
    This commit adds a new function `getListOfUserOwnedDevices` to the `DeviceRepository` interface. This function is designed to fetch a list of devices owned by the current user and returns a list of `OwnedDeviceResponse` objects.
    
    #104

--- app/src/main/java/com/sns/homeconnect_v2/domain/repository/DeviceRepository.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/DeviceRepository.kt
@@ -4,22 +4,15 @@ import com.sns.homeconnect_v2.data.remote.dto.request.ToggleRequest
 import com.sns.homeconnect_v2.data.remote.dto.response.AttributeResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.DeviceResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.LinkDeviceResponse
+import com.sns.homeconnect_v2.data.remote.dto.response.OwnedDeviceResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.ToggleResponse
 import com.sns.homeconnect_v2.data.remote.dto.response.UnlinkResponse
 
 interface DeviceRepository {
     suspend fun getInfoDevice(deviceId: Int): DeviceResponse
     suspend fun toggleDevice(deviceId: Int, toggleRequest: ToggleRequest): ToggleResponse
-    suspend fun updateAttributeDevice(
-        deviceId: Int,
-        brightness: Int,
-        color: String
-    ): AttributeResponse
-
+    suspend fun updateAttributeDevice(deviceId: Int, brightness: Int, color: String): AttributeResponse
     suspend fun unlinkDevice(deviceId: Int): UnlinkResponse
-    suspend fun linkDevice(
-        deviceId: String,
-        spaceId: String,
-        deviceName: String
-    ): LinkDeviceResponse
+    suspend fun linkDevice(deviceId: String, spaceId: String, deviceName: String): LinkDeviceResponse
+    suspend fun getListOfUserOwnedDevices(): List<OwnedDeviceResponse>
 }

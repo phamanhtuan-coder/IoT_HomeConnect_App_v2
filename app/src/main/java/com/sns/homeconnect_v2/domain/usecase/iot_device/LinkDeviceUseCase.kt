655652f2221e01b510f7dce20b4e6916466b8ce5
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-14 12:17:44 +0700

    refactor(iot_device): Update LinkDeviceUseCase to accept LinkDeviceRequest
    This commit modifies the `LinkDeviceUseCase` to accept a `LinkDeviceRequest` object as its input parameter instead of individual `deviceId`, `spaceId`, and `deviceName` strings.
    
    This change encapsulates the request parameters into a single object, improving code organization and making the use case more aligned with potential API changes.
    
    #106

--- app/src/main/java/com/sns/homeconnect_v2/domain/usecase/iot_device/LinkDeviceUseCase.kt
+++ app/src/main/java/com/sns/homeconnect_v2/domain/usecase/iot_device/LinkDeviceUseCase.kt
@@ -1,5 +1,6 @@
 package com.sns.homeconnect_v2.domain.usecase.iot_device
 
+import com.sns.homeconnect_v2.data.remote.dto.request.LinkDeviceRequest
 import com.sns.homeconnect_v2.data.remote.dto.response.LinkDeviceResponse
 import com.sns.homeconnect_v2.domain.repository.DeviceRepository
 import javax.inject.Inject
@@ -7,10 +8,10 @@ import javax.inject.Inject
 class LinkDeviceUseCase @Inject constructor(
     private val deviceRepository: DeviceRepository
 ) {
-    suspend operator fun invoke(deviceId: String, spaceId: String, deviceName: String): Result<LinkDeviceResponse> {
+    suspend operator fun invoke(request: LinkDeviceRequest): Result<LinkDeviceResponse> {
         return try {
-            val response = deviceRepository.linkDevice(deviceId, spaceId, deviceName)
-            Result.success(response)
+            val res = deviceRepository.linkDevice(request)
+            Result.success(res)
         } catch (e: Exception) {
             Result.failure(e)
         }

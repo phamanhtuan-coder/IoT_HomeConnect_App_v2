f90e4ed69fae321b544274e35ea943f10c9ba24e
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-06-15 16:48:41 +0700

    feat(repository): Define EcomRepository Interface
    This commit introduces the `EcomRepository` interface, which defines contracts for fetching e-commerce related data.
    
    Key changes:
    - Created `EcomRepository.kt`.
    - Added two suspend functions to the interface:
        - `getProductDetail(productId: Int): ProductData`: To fetch details of a specific product.
        - `getCategoryDetail(categoryId: Int): CategoryData`: To fetch details of a specific category.
    
    #111

---
+++ app/src/main/java/com/sns/homeconnect_v2/domain/repository/EcomRepository.kt
@@ -0,0 +1,9 @@
+package com.sns.homeconnect_v2.domain.repository
+
+import com.sns.homeconnect_v2.data.remote.dto.response.CategoryData
+import com.sns.homeconnect_v2.data.remote.dto.response.ProductData
+
+interface EcomRepository {
+    suspend fun getProductDetail(productId: Int): ProductData
+    suspend fun getCategoryDetail(categoryId: Int): CategoryData
+}
